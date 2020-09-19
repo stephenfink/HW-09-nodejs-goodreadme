586164232984ee242a6ebf33110ac89718277703
Stephen Fink <stephenfink8@gmail.com>
2020-09-19 11:25:53 -0500

    added a prompt function set up

--- index.js
+++ index.js
@@ -4,24 +4,44 @@
 //GIVEN a command-line application that accepts user input
 //WHEN I am prompted for information about my application repository
 //THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-    //prompt 1
+    
+    function prompt(){
+        return inquirer.prompt([
+            {
+                //prompt 1
         //WHEN I enter my project title
         //THEN this is displayed as the title of the README
-
-    //prompt 2
+            },
+            {
+                 //prompt 2
         //WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
         //THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-    //prompt 3
+            },
+            {
+                //prompt 3
         //WHEN I choose a license for my application from a list of options
         //THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-    //prompt 4
+            },
+            {
+                //prompt 4
          //WHEN I enter my GitHub username
         //THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 
-    //prompt 5
+            },
+            {
+                //prompt 4
+         //WHEN I enter my GitHub username
+        //THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+            },
+            {
+                 //prompt 5
         //WHEN I enter my email address
         //THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+            },
+        ])
+    }
+   
 //function 6        
     //WHEN I click on the links in the Table of Contents
     //THEN I am taken to the corresponding section of the README
